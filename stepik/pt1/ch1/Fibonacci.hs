7c96fe7ea6f1656f6389969c605492e6ba5c406a
Stanislav Solovyev <st.solp@gmail.com>
2020-11-07 17:48:14 +0300

    Edit fibonacci and fibonaccihelper

--- stepik/pt1/ch1/Fibonacci.hs
+++ stepik/pt1/ch1/Fibonacci.hs
@@ -19,3 +19,10 @@ F_−10=−55
 Измените определение функции fibonacci так, чтобы она была определена для всех целых чисел и порождала при отрицательных аргументах указанную последовательность.
 -}
 
+fibonacci :: Integer -> Integer
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n == -1 = 1
+            | n == -2 = -1
+            | n > 1 = fibonacci (n - 1) + fibonacci (n - 2)
+            | otherwise = fibonacci (n + 2) - fibonacci (n + 1)
