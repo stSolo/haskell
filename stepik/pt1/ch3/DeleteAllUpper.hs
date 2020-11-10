8a77ab57ad9776b14873df33e5c391912537490e
Stanislav Solovyev <st.solp@gmail.com>
2020-11-10 21:39:40 +0300

    Add a new task - delAllUpper

---
+++ stepik/pt1/ch3/DeleteAllUpper.hs
@@ -0,0 +1,18 @@
+{-
+Реализуйте функцию delAllUpper, удаляющую из текста все слова, целиком состоящие из символов в верхнем регистре. Предполагается, что текст состоит только из символов алфавита и пробелов, знаки пунктуации, цифры и т.п. отсутствуют.
+
+GHCi> delAllUpper "Abc IS not ABC"
+"Abc not"
+Постарайтесь реализовать эту функцию как цепочку композиций, аналогично revWords из предыдущего видео.
+-}
+module DeleteAllUpper where
+
+import Data.Char
+
+delAllUpper :: String -> String
+delAllUpper = unwords . filter (any isLower) . words
+{-
+delAllUpper = unwords . filter ( > "") . map deleter . words
+    where deleter w | all (isUpper) w = ""
+                    | otherwise = w
+-}
