7413536295175521fdfc8ba07685961f92ac3207
Stanislav Solovyev <st.solp@gmail.com>
2020-11-12 19:27:41 +0300

    Add a new task - evenOnly2

--- stepik/pt1/ch3/EvenOnly.hs
+++ stepik/pt1/ch3/EvenOnly.hs
@@ -9,3 +9,14 @@ GHCi> evenOnly ['a'..'z']
 
 evenOnly :: [a] -> [a]
 evenOnly = reverse . snd . foldl (\(n,ar) x -> (n+1,if even n then x:ar else ar)) (1,[])
+
+
+{-
+evenOnly = snd . foldr (\x (ar,ar2) -> ((x:ar2, ar))) ([],[])
+-}
+
+
+{-
+evenOnly :: [a] -> [a]
+evenOnly = (\(q, (a,a2)) -> if odd q then a2 else a) . foldr (\x (n,(ar,ar2)) -> (n+1,if even n then (x:ar, ar2) else (ar, x:ar2) )) (1,([],[]))
+-}
