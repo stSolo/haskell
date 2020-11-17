6e5bfa181ba9f0df52ee60de31d380cd8129ec4f
Stanislav Solovyev <st.solp@gmail.com>
2020-11-17 19:37:58 +0300

    Add a new task - Nat

---
+++ stepik/pt1/ch4/Nat.hs
@@ -0,0 +1,35 @@
+{-
+Рассмотрим еще один пример рекурсивного типа данных:
+
+data Nat = Zero | Suc Nat
+Элементы этого типа имеют следующий вид: Zero, Suc Zero, Suc (Suc Zero), Suc (Suc (Suc Zero)), и так далее. Таким образом мы можем считать, что элементы этого типа - это натуральные числа в унарной системе счисления.
+
+Мы можем написать функцию, которая преобразует Nat в Integer следующим образом:
+
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+Реализуйте функции сложения и умножения этих чисел, а также функцию, вычисляющую факториал.
+-}
+
+data Nat = Zero | Suc Nat deriving Show
+
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+
+toNat :: Integer -> Nat
+toNat 0 = Zero
+toNat n = Suc (toNat $ n-1)
+
+
+add :: Nat -> Nat -> Nat
+add x y = toNat $ fromNat x + fromNat y
+
+mul :: Nat -> Nat -> Nat
+mul x y = toNat $ fromNat x * fromNat y
+
+fac :: Nat -> Nat
+fac x = toNat $ helper $ fromNat x
+  where helper 0 = 1
+        helper n = n * (helper $ n - 1)
